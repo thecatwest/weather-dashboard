a5f176a882878bed3cc59af3ac5fb53842619ef4
TheCatWest <catwestover@outlook.com>
2021-06-01 12:33:43 -0700

    file structure and pseudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// **TO DO**
+// access OpenWeather API using GET
+// capture user input and add to API call for search
+// display populated search data in main div for current weather
+// display populated search data in cards for 5-day forecast
+// set searched cities into localStorage
+// retrieve and populate cities from localStorage and display on sidebar
