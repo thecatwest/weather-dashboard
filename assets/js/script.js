4c4cc4738d82d62314f48cd82a4e0e71be61f349
TheCatWest <catwestover@outlook.com>
2021-06-01 14:59:52 -0700

    set up data variables; set up button event

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,31 @@
 // display populated search data in cards for 5-day forecast
 // set searched cities into localStorage
 // retrieve and populate cities from localStorage and display on sidebar
+
+// button variables
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+
+// capture forecast variables
+var cityName = document.querySelector("#search-input");
+var date = document.querySelector(".date");
+var description = document.querySelector(".description");
+var weatherIcon = document.querySelector(".weatherIcon");
+var temperature = document.querySelector(".temperature");
+var wind = document.querySelector(".wind");
+var humidity = document.querySelector(".humidity");
+var uvIndex = document.querySelector(".uvIndex");
+
+// data for each daily forecast
+
+
+
+
+// search event listener
+searchButton.addEventListener("click", () => {
+    // on click, capture searchInput value and save as variable to use
+    const inputValue = searchInput.value;
+    //   put into api call?
+    // retrieve data and display?
+    alert(inputValue);
+});
