cf0310f732be52aae1ba17aebae441db06ce883d
TheCatWest <catwestover@outlook.com>
2021-06-08 19:01:23 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# weather-dashboard
+# Readme Generator
+
+<!-- Project Table of Contents -->
+<details>
+  <h2 class="display-inline-block">Description</h2>
+  This app generates a professional README.md file for projects.
+
+  <summary>
+  <h2 class="display-inline-block">Table of Contents</h2>
+  </summary>
+  <ul>
+    <li><a href="#about-project">About The Project</a></li>
+    <li><a href="#projectInstall">Installation</a></li>
+    <li><a href="#links">Project Links</a></li>
+    <li><a href="#inquiries">Inquiries</a></li>
+  </ul>
+</details>
+
+<!-- About Project Section -->
+## About The Project
+This app allows users to explore the weather in a city by entering a city name. The previously searched cities are saved on the left-hand side of the app, allowing users to see previously searched cities to easily review the weather forecast in that location.
+
+Anyone may use this app.
+
+https://thecatwest.github.io/weather-dashboard/
+
+<h5 class="text-dark">
+&copy; 2021 by Cat Westover
+</h5>
+
+Project Technologies:
+
+ES6,JavaScript,CSS,Bootstrap
+
+<!-- Installation -->
+## Installation
+
+No installation necessary. Simply navigate to the app's live url
+
+<!-- Inquiries -->
+## Inquiries
+
+GitHub Username: thecatwest
+
+Name: Cat Westover
+
+email: catwestover@outlook.com
+
+## License
+    Project licensed under undefined
+
+[License](#license) 
+
+![License: undefined](https://img.shields.io/badge/License-undefined-yellow.svg)
+
+<!-- Project Badges -->
+## Project Badges
+
+undefined
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // access OpenWeather API using GET
 // capture user input and add to API call for search
 // display populated search data in main div for current weather
@@ -6,141 +5,164 @@
 // set searched cities into localStorage
 // retrieve and populate cities from localStorage and display on sidebar
 
-var searchButton = document.querySelector("#search-button");
-
-const forecast = function(city, lat, lon) {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=a1acf7ffaf63eef475d50de8d0125471")
-    .then(function(response){
-        return response.json();
-    }).then(function(data) {
-        // wipe previous data first by setting text to empty string
-        $("#current-weather-display").text("");
-
-        // set city and weather data search to localStorage
-        saveSearch(city);
-        // clear search input
-        $("#search-input").val("");
-
-        // set weather icon as variable
-        var weatherIcon = data.current.weather[0].icon;
-        // retrieve current date using Moment.js
-        var date = moment().format("MM/DD/YY");
-
-        // create containers and append data
-        $("<div>")
-        .addClass("card text-white bg-info mb-3")
-        .attr("id", "current-weather-data")
-        .appendTo("#current-weather-display");
-
-        // retrieve current weather icon
-        $("#current-weather-icon")
-        .attr("src", "https://openweathermap.org/img/w/" + weatherIcon + ".png")
-        
-        // append general city info to heading
-        $("<h2>")
-        .addClass("mb-3")
-        .html(cityNameMain + " -" + date + "<img id='current-weather-icon' src='' alt='Current Weather Icon'>")
-        .appendTo("#current-weather-data");
-
-        // create and append <p> elements into #current-weather-display
-        // create and append temperature
-        $("<p>")
-        .text("`Temperature: ${data.current.temp}°F`")
-        .appendTo("#current-weather-data");
-
-        // create and append wind speed
-        $("<p>")
-        .text("`Wind Speed: ${data.current.wind_speed}`")
-        .appendTo("#current-weather-data");
-
-        // create and append humidity
-        $("<p>")
-        .text("`Humidity: ${data.current.humidity}`")
-        .appendTo("#current-weather-data");
-
-        // create and append UV index
-        $("<p>")
-        .text("`UV Index: ${data.current.uvi}`")
-        .appendTo("#current-weather-data");
-        
-        // 5-Day Forecast container
-        $("<div>")
-        .addClass("5-day-forecast mt-4")
-        .appendTo("#current-weather-display")
-
-        // forecast heading
-        $("<h2>")
-        .text("Upcoming 5-Day Forecast")
-        .appendTo(".5-day-forecast");
-
-        // create and append card container to 5-day-forecast
-        $("<div>")
-        .addClass("row card-container")
-        appendTo(".5-day-forecast");
-
-        // implement for counting loop for 5-day forecast cards
-        for (let index = 0; index < 5; index++) {
-            // create, append various elements to forecast card
-            // card div
-            $("<div>")
-            .addClass("card pb-2 col-3 text-white bg-info")
-            .attr("id", "forecast-card" + index)
-            .appendTo(".card-container");
-
-            // card body
-            $("<div>")
-            .addClass("forecast-card-body pl-1 col")
-            .attr("id", "forecast-card-body" + index)
-            .appendTo("#forecast-card" + index);
-
-            // weather icon <p>
-            $("<p>")
-            .addClass("forecast-card-text")
-            .attr("<img src='https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png' alt='Current Weather Icon'>")
-            .appendTo("#forecast-card" + index);
-
-            // header
-            $("<h2>")
-            .text(moment().add(index, 'd').format("MM/DD/YY"))
-            .addClass("forecast-card-header")
-            // .attr("")
-            .appendTo("#forecast-card-body" + index);
-
-            // temp <p>
-            $("<p>")
-            .text("`Temp: ${data.daily[index].temp.day}°F`")
-            .addClass("forecast-card-text")
-            .appendTo("#forecast-card");
-
-            // humidity <p>
-            $("<p>")
-            .text("`Humidity: ${data.daily[index].humidity}%`")
-            .addClass("forecast-card-text")
-            .appendTo("#forecast-card");
-        }
-    })
-}
+var savedSearch = document.querySelector("#saved-searches");
+const savedSearches = JSON.parse(localStorage.getItem("savedCities")) || [];
 
 // API call to search by city
-const searchByCity = function(cityData) {
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityData + "&limit=1&APPID=a1acf7ffaf63eef475d50de8d0125471")
-    // if successful, return response in JSON to pull lat/lon from
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        // capture lat/lon data with variables
-        var lat = data[0].lat;
-        var lon = data[0].lon;
+const searchByCity = function (city) {
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=a1acf7ffaf63eef475d50de8d0125471")
+        // if successful, return response in JSON to pull lat/lon from
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // capture lat/lon data with variables
+            var lat = data[0].lat;
+            var lon = data[0].lon;
 
-        getWeather(cityData, lat, lon);
-    });
+            getWeather(city, lat, lon);
+        });
 };
 
+var getWeather = function (city, lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=a1acf7ffaf63eef475d50de8d0125471")
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            // wipe previous data first by setting text to empty string
+            $("#current-weather-display").text(""); // wipe previous data first by setting text to empty string
+            $("#days-forecast").text("");
+
+            // set city and weather data search to localStorage
+            saveSearch(city);
+            // clear search input
+            $("#search-input").val("");
+
+            // set weather icon as variable
+            var weatherIcon = data.current.weather[0].icon;
+            // retrieve current date using Moment.js
+            var date = moment().format("MM/DD/YY");
+
+            // create containers and append data
+            $("<div>")
+                .addClass("card text-white bg-info mb-3")
+                .attr("id", "current-weather-data")
+                .appendTo("#current-weather-display");
+
+            // append general city info to heading
+            $("<h2>")
+                .addClass("mb-3")
+                .html(city + " - " + date + "<img id='current-weather-icon' src='' alt='Current Weather Icon'>")
+                .appendTo("#current-weather-data");
+
+            // retrieve current weather icon
+            $("#current-weather-icon")
+                .attr("src", "https://openweathermap.org/img/w/" + weatherIcon + ".png")
+
+            // create and append <p> elements into #current-weather-display
+            // create and append temperature
+            $("<p>")
+                .text("Temperature:" + data.current.temp + "°F")
+                .appendTo("#current-weather-data");
+
+            // create and append wind speed
+            $("<p>")
+                .text("Wind Speed:" + data.current.wind_speed)
+                .appendTo("#current-weather-data");
+
+            // create and append humidity
+            $("<p>")
+                .text("Humidity:" + data.current.humidity)
+                .appendTo("#current-weather-data");
+
+            // create and append UV index
+            $("<p>")
+                .text("UV Index:" + data.current.uvi)
+                .appendTo("#current-weather-data");
+
+            // 5-Day Forecast container
+            $("<div>")
+                .addClass("mt-4 days-forecast")
+                .attr("id", "#days-forecast")
+                .appendTo("#current-weather-display");
+
+            // forecast heading
+            $("<h2>")
+                .text("Upcoming Weather")
+                .appendTo(".days-forecast");
+
+            // create and append card container to days-forecast
+            $("<div>")
+                .addClass("row card-container justify-content")
+                .appendTo(".days-forecast");
+
+            // implement for counting loop for 5-day forecast cards
+            for (index = 0; index < 5; index++) {
+                // create, append various elements to forecast card
+                // card div
+                $("<div>")
+                    .addClass("card pb-2 col-3 text-white bg-info")
+                    .attr("id", "forecast-card" + index)
+                    .appendTo(".card-container");
+
+                // card body
+                $("<div>")
+                    .addClass("card-body pl-1 col")
+                    .attr("id", "forecast-card-body" + index)
+                    .appendTo("#forecast-card" + index);
+
+                // header
+                $("<h2>")
+                    .text(moment().add(index, 'd').format("MM/DD/YY"))
+                    .addClass("forecast-card-header")
+                    .appendTo("#forecast-card-body" + index);
+
+                // weather icon <p>
+                // $("<p>")
+                //     .addClass("forecast-card-text")
+                //     .html(city +
+                //         "<img id='current-weather-icon' src='' alt='Current Weather Icon'>")
+                //     .appendTo("#forecast-card" + index);
+
+                // $("#current-weather-icon")
+                //     .attr("src", "https://openweathermap.org/img/w/" + weatherIcon + ".png")
+
+                // temp <p>
+                $("<p>")
+                    .text("Temp: " + data.daily[index].temp.day + "°F")
+                    .addClass("forecast-card-text")
+                    .appendTo("#forecast-card" + index);
+                // console.log(index);
+
+                // humidity <p>
+                $("<p>")
+                    .text("Humidity: " + data.daily[index].humidity + "%")
+                    .addClass("forecast-card-text")
+                    .appendTo("#forecast-card" + index);
+                // console.log(data.daily[index].humidity);
+            };
+        });
+};
+
+// var getFiveDay = function (city) {
+//     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=a1acf7ffaf63eef475d50de8d0125471")
+//         .then(function (response) {
+//             return response.json();
+//         }).then(function (data) {
+//             console.log("5 day data", data);
+//         })
+//         var day1Icon = data.list.weather.icon;
+//         console.log(day1Icon);
+//         console.log(data.list.weather);
+//         var dailyData = (data.list[index].weather[0].icon)
+// };
+
+
+
 // set city searches into localStorage
-const saveSearch = function(data) {
+const saveSearch = function (data) {
     for (let index = 0; index < savedSearches.length; index++) {
-        if (savedSearches[index] === data) {
+        if ((savedSearches[index]) === data) {
             return;
         }
     };
@@ -148,65 +170,41 @@ const saveSearch = function(data) {
     localStorage.setItem("savedCities", JSON.stringify(savedSearches));
 };
 
-// city search button functionality
-$("#search-button").on("click", function() {
-    event.preventDefault();
+// create clickable buttons for saved city searches to access data
+var savedButtons = function () {
+    for (let index = 0; index < savedSearches.length; index++) {
+        $("<button>")
+            .text(savedSearches[index])
+            .addClass("btn saved-btn p-1 col-12 text-white btn-info")
+            .appendTo("#saved-searches");
+    }
 
-    var cityData = $("#search").val()
+    // add event listener for button click
+    $(".saved-btn").on("click", function () {
+        console.log("this button doesn't suck")
+        // capture button text, set as variable, call in searchByCity function
+        var savedCityData = $(this).text();
+
+        searchByCity(savedCityData);
+        // getFiveDay(savedCityData);
+    });
+};
+
+// city search button functionality
+$("#search-button").on("click", function () {
+    event.preventDefault();
+    console.log("things were clicked");
+
+    var cityData = $("#search-input").val()
+    console.log(cityData);
 
     // pass cityData into the searchByCity() fn
     searchByCity(cityData);
 });
 
 
+var savedCities = localStorage.getItem("savedCities");
 
-// create clickable buttons for saved city searches to access data
-var savedButtons = function(buttonsArray) {
-    for (let index = 0; index < buttonsArray.length; index++) {
-        $("<button>")
-        .text(buttonsArray[index])
-        .addClass("btn saved-btn p-1 col-12 text-white btn-info")
-        .appendTo("#saved-searches");
-    }
+savedButtons(savedCities);
 
-    // add event listened for button click
-    $(".saved-button").on("click", function() {
-        // capture button text, set as variable, call in searchByCity function
-        var savedCityData = $(this).text();
-
-        searchByCity(savedCityData);
-    });
-};
-
-savedButtons(saveSearch);
-
-// // search event listener
-// searchButton.addEventListener("click", () => {
-//     // on click, capture searchInput value and save as variable to use
-//     const inputValue = searchInput.value;
-//     //   put into api call?
-//     // retrieve data and display?
-//     const response =
-//     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&units=metric&id=524901&APPID=a1acf7ffaf63eef475d50de8d0125471`)
-
-//     if (response.status === 200) {
-//         const data = response.json()
-//         return data
-//     } else {
-//         throw new Error("An error has occurred. Unable to complete weather request.")
-//     }
-// });
-
-// const getWeather = async(inputValue) => {
-//     const response = await
-//     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=a1acf7ffaf63eef475d50de8d0125471`)
-
-//     if (response.status === 200) {
-//         const data = await response.json()
-//         return data
-//     } else {
-//         throw new Error("An error has occurred. Unable to complete weather request.")
-//     }
-// };
-
-const savedSearches = JSON.parse(localStorage.getItem("savedCities")) || [];
+// console.log(savedCities);
